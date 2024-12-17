06325d9a9b361b0507d222d2af665823c24f8f62
Henrique Camargo nobrega <Henriquecnobrega0711@gmail.com>
2024-12-17 18:22:44 -0300

    App Finalizado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.alcoolougasolina"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.alcoolougasolina"
